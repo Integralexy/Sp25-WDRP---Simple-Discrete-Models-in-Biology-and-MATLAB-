2282f8e0a0e0dfc66fc056bc01fed81b1c6bf4ca
Integralexy <aaguilar7294@gmail.com>
2025-04-06 02:50:33 -0700

    Update func.m

--- func.m
+++ func.m
@@ -1,20 +1,21 @@
 Num = randi([1,10]);
-Range = 1:10;
+Range = 1:1:10;
 Past = [0];
 check = 0;
 IndP = 0;
-IndR = 0;
+
 
 while check == 0
     Guess = GetGuess();
     
+    IndR = 1;
     while IndR == 1
         [IndR, Guess, Past] = CheckRange(Range, Guess, Past); 
     end
     
     [Guess, Past] = CheckPrevGuess(Past,Guess);
     check = CheckGuess(Num,Guess);
-    Past = [Past Guess];
+    Past = [Past Guess]
 end
 
 fprintf('You got it! The number was %1.0f \n', Num);
@@ -34,13 +35,12 @@ end
 
 %============================================%
 function [I,g,P] = CheckRange(R,g,P)
-    I = g < R(1) && g > R(length(R));
+    I = g < R(1) || g > R(length(R));
 
     while I == 1
         fprintf('Your guess was not in the correct range. \n')
         g = GetGuess();
-        P = [P g];
-        I = g < R(1) && g > R(length(R));
+        I = g < R(1) || g > R(length(R));
     end
 end
 
@@ -48,16 +48,17 @@ end
 function [g,P,I] = CheckPrevGuess(P,g)
 I = 0;
 i = 1;
-    while I == 0 && i <= length(P)
+    while length(P) > 0 && I == 0 && i <= length(P)
         I = P(i) == g;
         i = i + 1;
     end
 
+    
     while I == 1
         fprintf('You have already guessed that, try again \n');
         g = GetGuess();
         [P,g,I] = CheckPrevGuess(P,g);
-        P = [P, g];
+        P = [P, g]
     end
 end
 
